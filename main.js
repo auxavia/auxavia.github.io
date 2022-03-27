69ea8afaada5f889f9fdc8a338b03d64ff7a444a
auxavia <99677038+auxavia@users.noreply.github.com>
2022-03-28 00:14:16 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+// Navbar //
+const hamburger = document.querySelector('.hamburger')
+
+hamburger.addEventListener('click', function () {
+    this.classList.toggle('isActive');
+})
+
+// Characters //
+
+const draggables = document.querySelectorAll('.draggable')
+const containers = document.querySelectorAll('.charactercontainer')
+
+draggables.forEach(draggable => {
+  draggable.addEventListener('dragstart', () => {
+    draggable.classList.add('dragging')
+  })
+
+  draggable.addEventListener('dragend', () => {
+    draggable.classList.remove('dragging')
+  })
+})
+
+containers.forEach(container => {
+  container.addEventListener('dragover', e => {
+    e.preventDefault()
+    const afterElement = getDragAfterElement(container, e.clientY)
+    const draggable = document.querySelector('.dragging')
+    if (afterElement == null) {
+      container.appendChild(draggable)
+    } else {
+      container.insertBefore(afterElement)
+    }
+  })
+})
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect()
+    const offset = y - box.top - box.height / 2
+    if (offset < 0 && offset > closest.offset) {
+      return { offset: offset, element: child }
+    } else {
+      return closest
+    }
+  }, { offset: Number.NEGATIVE_INFINITY }).element
+}
+
+// Spinner //
+let container = document.querySelector(".container");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 1000);
+
+btn.onclick = function () 
+{
+	container.style.transform = "rotate(" + number + "deg)";
+	number += Math.ceil(Math.random() * 1000);
+}
